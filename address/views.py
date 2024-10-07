3620443e621fa013108e8525a25fe411aaf2b655
cp-operacional <operacional@contactpro.com.br>
2024-10-07 11:29:40 -0300

    feat: filter cities and get ufs views

--- address/views.py
+++ address/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Municipios, Estados
 
-# Create your views here.
+class FiltrarMunicipios(APIView):
+    def post(self, request):
+        codigo_uf = request.data.get('codigo_uf')
+        if codigo_uf is None:
+            return Response({"error": "codigo_uf é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+
+        municipios = Municipios.objects.filter(codigo_uf=codigo_uf)
+        return Response({"municipios": list(municipios.values_list('nome', flat=True))}, status=status.HTTP_200_OK)
+
+class ListarUfs(APIView):
+    def get(self, request):
+        ufs = Estados.objects.values('codigo_uf', 'uf')
+        return Response(list(ufs), status=status.HTTP_200_OK)
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from indeferidos.views import IndeferidosListView, IndeferidosCountView
+from address.views import FiltrarMunicipios, ListarUfs
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("indeferidos/", IndeferidosListView.as_view()),
     path("count/", IndeferidosCountView.as_view()),
+    path('municipios/filtrar/', FiltrarMunicipios.as_view(), name='filtrar_municipios'),
+    path('ufs/', ListarUfs.as_view(), name='listar_ufs'),
 ]
